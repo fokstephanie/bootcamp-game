c78a6e1cd583cc20d8fdb553a7d46100631e1992
Stephanie Fok <the.stephanie.fok@gmail.com>
2019-01-08 15:00:01 -0500

    Blocks snap back to original position

--- Bootcamp-Game/Assets/Scripts/DragBlock.cs
+++ Bootcamp-Game/Assets/Scripts/DragBlock.cs
@@ -8,18 +8,16 @@ public class DragBlock : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
 
 	private bool dragging = false;
 	private Vector3 newPosition;
-	public Vector3 originalPosition;
+	private Vector3 originalPosition;
 
 	public void OnPointerDown(PointerEventData eventData)
 	{
-		Debug.Log("OnPointerDown");
 		// newPosition = Vector3.Distance(transform.position, Camera.main.transform.position);
 		dragging = true;
 	}
 
 	public void OnPointerUp(PointerEventData eventData)
 	{
-		Debug.Log("OnPointerUp");
 		dragging = false;
 		transform.localPosition = originalPosition;
 	}
@@ -35,15 +33,13 @@ public class DragBlock : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
 			// Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			// Vector3 rayPoint = ray.GetPoint (distance);
 			// transform.position = rayPoint;
-			transform.localPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+			transform.position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 		} 
 	}
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Start");
 		originalPosition = transform.localPosition;
-		Debug.Log("originalPosition = " + originalPosition);
 	}
 
 }
