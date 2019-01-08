b5dcaa1b14feca89f83d680d02637dcbc46632f2
Stephanie Fok <the.stephanie.fok@gmail.com>
2019-01-08 14:34:34 -0500

    Changing blocks positions

--- Bootcamp-Game/Assets/Scripts/DragBlock.cs
+++ Bootcamp-Game/Assets/Scripts/DragBlock.cs
@@ -1,16 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class DragBlock : MonoBehaviour {
+public class DragBlock : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	private bool dragging = false;
+	private Vector3 newPosition;
+	public Vector3 originalPosition;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Debug.Log("OnPointerDown");
+		// newPosition = Vector3.Distance(transform.position, Camera.main.transform.position);
+		dragging = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		Debug.Log("OnPointerUp");
+		dragging = false;
+		transform.position = originalPosition;
+	}
+
+	public void OnDrag() {
+	
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (dragging) {
+			// Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			// Vector3 rayPoint = ray.GetPoint (distance);
+			// transform.position = rayPoint;
+			transform.position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		} 
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log("Start");
+		originalPosition = transform.posi;
+		Debug.Log("originalPosition = " + originalPosition);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
