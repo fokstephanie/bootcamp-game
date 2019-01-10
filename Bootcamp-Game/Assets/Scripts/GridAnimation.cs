b86cef0c2ba7c1fcf43bced6ff363938f2bf95ef
Stephanie Fok <the.stephanie.fok@gmail.com>
2019-01-10 11:49:39 -0500

    Added menu and menu animations

---
+++ Bootcamp-Game/Assets/Scripts/GridAnimation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridAnimation : MonoBehaviour {
+	
+	public GameObject grid;
+	public GameObject[,] gridArray = new GameObject[8, 8];
+	public GameObject blackScreen;
+	private float fadeTime = 0.8f;
+	private float initialDelay = 0.5f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		int rowCount = 0;
+		int colCount = 0;
+		
+		blackScreen.SetActive(true);
+		
+		// Fade in blackScreen
+		LeanTween.alpha(blackScreen.GetComponent<RectTransform>(), 0, fadeTime).setEaseInCubic()
+			.setOnComplete(() => blackScreen.SetActive(false));
+		
+		// Populate gridColourArray
+		foreach (Transform child in transform)
+		{
+			foreach (Transform childTransform in child)
+			{
+				gridArray[rowCount, colCount] = childTransform.gameObject;
+				colCount++;
+			}
+			colCount = 0;
+			rowCount++;
+		}
+
+		// Set default size of all squares to 0
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				LeanTween.scale(gridArray[i, j], Vector2.zero, 0);
+			}
+		}
+		
+		// Animate-in squares
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				LeanTween.scale(gridArray[i, j], Vector2.one, 0.6f).setDelay((0.08f * (i + j)) + fadeTime).setEaseOutCubic()
+					.setOnComplete(EnableColliders);
+			}
+		}
+	}
+
+	private void EnableColliders()
+	{
+		foreach (GameObject obj in gridArray)
+		{
+			obj.GetComponent<BoxCollider2D>().enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Bootcamp-Game/Assets/Scripts/MenuAnimations.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+using Button = UnityEngine.UI.Button;
+
+public class MenuAnimations : MonoBehaviour
+{
+
+	public GameObject left8;
+	public GameObject right8;
+	public GameObject blackScreen;
+	public GameObject playButton;
+	public GameObject playSymbol;
+
+	// Use this for initialization
+	void Start () {
+		float titleDelay = 0.5f;
+		float titleAnimationTime = 1.4f;
+		float new8XPosition = 69.5f;
+		float fadeTime = 1.1f;
+		float buttonFinalPosition = 102;
+	
+		blackScreen.SetActive((false));
+		playButton.GetComponent<Button>().interactable = false;
+		
+		// Set initial values
+		LeanTween.alpha(left8.GetComponent<RectTransform>(), 0, 0);
+		LeanTween.alpha(right8.GetComponent<RectTransform>(), 0, 0);
+		LeanTween.alpha(playButton.GetComponent<RectTransform>(), 0, 0);
+		LeanTween.alpha(playSymbol.GetComponent<RectTransform>(), 0, 0);
+		LeanTween.moveLocalX(left8, 0, 0);
+		LeanTween.moveLocalX(right8, 0, 0);
+		LeanTween.moveY(playButton, buttonFinalPosition - 80, 0);
+		LeanTween.moveLocalY(playSymbol, 170, 0);
+		
+		// Animate 8's
+		// Fade in
+		LeanTween.alpha(left8.GetComponent<RectTransform>(), 1, fadeTime).setEaseInQuint();
+		LeanTween.alpha(right8.GetComponent<RectTransform>(), 1, fadeTime).setEaseInQuint();
+		// Separate
+		LeanTween.moveLocalX(left8, -new8XPosition, titleAnimationTime).setDelay(titleDelay + (fadeTime * 0.35f)).setEaseOutQuad();
+		LeanTween.moveLocalX(right8, new8XPosition, titleAnimationTime).setDelay(titleDelay + (fadeTime * 0.35f)).setEaseOutQuad();
+		
+		// Animate-in playButton
+		// Fade in
+		LeanTween.alpha(playButton.GetComponent<RectTransform>(), 1, 0.8f).setDelay(titleDelay + 0.3f + (fadeTime * 0.35f)).setEaseOutQuad();
+		LeanTween.alpha(playSymbol.GetComponent<RectTransform>(), 1, 0.8f).setDelay(titleDelay + 0.3f + (fadeTime * 0.35f)).setEaseOutQuad();
+		// Move up
+		LeanTween.moveY(playButton, buttonFinalPosition, 0.8f).setDelay(titleDelay + (fadeTime * 0.35f)).setEaseOutQuad();
+		LeanTween.moveLocalY(playSymbol, 200, 0.8f).setDelay(titleDelay + (fadeTime * 0.35f)).setEaseOutQuad()
+			.setOnComplete(() => playButton.GetComponent<Button>().interactable = true);
+	}
+
+	public void Play()
+	{
+		blackScreen.SetActive((true));
+		LeanTween.alpha(blackScreen.GetComponent<RectTransform>(), 0, 0);
+		
+		// Animate blackScreen fade, switch scenes
+		LeanTween.alpha(blackScreen.GetComponent<RectTransform>(), 1, 0.8f).setEaseInCubic()
+			.setOnComplete(LoadScene);
+		
+	}
+
+	private void LoadScene()
+	{
+		blackScreen.SetActive(false);
+		SceneManager.LoadScene("Main");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
